802fb991c405cb68ab4b7d224a153687593fed8a
ReaperClown <the.willian.mesquita@gmail.com>
2021-08-29 21:27:17 -0300

    Created exception classes

--- src/main/java/com/manager/room/meeting/meetingroom/exception/ResourceNotFoundException.java
+++ src/main/java/com/manager/room/meeting/meetingroom/exception/ResourceNotFoundException.java
@@ -1,5 +1,14 @@
 package com.manager.room.meeting.meetingroom.exception;
 
-public class ResourceNotFoundException {
-    
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception {
+
+    private static final Long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
